6c2bb83b9593d97df20a9b01cb81973f063b6d86
Sammy Antigua <samiami83@hotmail.com>
2021-03-18 08:59:35 -0400

    put the sales data into table format

--- js/app.js
+++ js/app.js
@@ -36,6 +36,7 @@ let tokyo = new CookieStand('Tokyo', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
 // }
 
 let amman = new CookieStand('Amman', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]);
+let locations = [seattle, tokyo, amman];
 
 // const amman = {
 //   location: 'Amman',
@@ -44,59 +45,126 @@ let amman = new CookieStand('Amman', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
 
 const storeContainerElem = document.getElementById('store-container');
 
-renderCookieStand(seattle);
-renderCookieStand(tokyo);
-renderCookieStand(amman);
+// renderCookieStand(seattle);
+// renderCookieStand(tokyo);
+// renderCookieStand(amman);
+
+const tableBodyElem = document.createElement('tbody');
+const tableFootElem = document.createElement('tfoot');
+const tableHeadElem= document.createElement('thead');
+const cellElem = document.createElement('td');
+// const headCellElem = document.createElement('th');
+const rowElem = document.createElement('tr');
+const tableElem = document.createElement('table');
 
 function renderCookieStand(cookieStand) {
 
-  const sectionElem = document.createElement('table');
-  storeContainerElem.appendChild(sectionElem);
-
-  // const sectionElem = document.createElement('section');
-  // storeContainerElem.appendChild(sectionElem);
-
-  const locationElem = document.createElement('tr');
-  sectionElem.appendChild(locationElem);
-  locationElem.textContent = cookieStand.location;
-
-  // const locationElem = document.createElement('h2');
-  // sectionElem.appendChild(locationElem);
-  // locationElem.textContent = cookieStand.location;
-
-  const hourListElem = document.createElement('th');
-  sectionElem.appendChild(hourListElem);
-
-  // const hourListElem = document.createElement('ul');
-  // sectionElem.appendChild(hourListElem);
+  storeContainerElem.appendChild(tableElem);
+  tableElem.appendChild(tableHeadElem);
+  tableHeadElem.appendChild(rowElem);
 
   for (let i = 0; i < timeSlots.length; i += 1) {
-
-    const hourItemElem = document.createElement('td');
-
-    // const hourItemElem = document.createElement('li');
-
-    hourListElem.appendChild(hourItemElem);
+    const headCellElem = document.createElement('th');
+    rowElem.appendChild(headCellElem);
 
     const currentTimeSlot = timeSlots[i];
 
-    const currentSales = cookieStand.hourlySales[i];
+    // const currentSales = cookieStand.hourlySales[i];
 
-    hourItemElem.textContent = currentTimeSlot + ': ' + currentSales + ' cookies';
-
+    headCellElem.textContent = currentTimeSlot;
   }
+  tableElem.appendChild(tableBodyElem);
+
+  for (let i = 0; i < locations.length; i += 1) {
+    const rowElem = document.createElement('tr');
+    tableBodyElem.appendChild(rowElem);
+// console.log(locations[i].hourlySales);
+    for (let cookieInd = 0; cookieInd < locations[i].hourlySales[cookieInd]; cookieInd += 1) {
+      const cellElem = document.createElement('td');
+      // console.log(locations[i][1]);
+      rowElem.appendChild(cellElem);
+
+      const currentCookie = locations[i].hourlySales[cookieInd];
+      cellElem.textContent = currentCookie;
+    }}
+
+  //   <!-- header row -->
+  //   <thead>  <!-- hard code code this -->
+  //       <tr> <!-- hard code code this -->
+  //           <!-- for loop times -->
+  //               <th> 1 </th>
+  //               <th> 2 </th>
+  //               <!-- end for loop times -->
+  //       </tr>
+  //   </thead>
+  //   <!-- End Header -->
+
+  //   <!-- Start table cells -->
+  //   <tbody>  <!-- hard code code this -->
+  //       <!-- for loop locations objects -->
+  //       <tr>
+  //           <!-- for loop for # of cookies -->
+  //           <td> 1 </td>
+  //           ...
+  //       </tr>
+  //       ...
+  //       <!-- END for loop locations objects -->
+  //   </tbody>
+  //   <!-- End Table Cells -->
+
+  //   <!-- Start Footer  -->
+  //   <!-- for loop locations object -->
+  //   <tfoot>
+  //       <tr>
+
+  //       </tr>
+  //   </tfoot>
+  //   <!-- End footer  -->
 
 
-  let total = 0;
-  // the sum of all the sales for the current cookieStand
 
-  for (let i = 0; i < cookieStand.hourlySales.length; i += 1) {
-    const currentSales = cookieStand.hourlySales[i];
-    total += currentSales;
-  }
-  // need last total item
-  const totalItemElem = document.createElement('li');
-  hourListElem.appendChild(totalItemElem);
-  totalItemElem.textContent = 'Total: ' + total + ' cookies';
+  // // const sectionElem = document.createElement('section');
+  // // storeContainerElem.appendChild(sectionElem);
+
+  // sectionElem.appendChild(locationElem);
+  // locationElem.textContent = cookieStand.location;
+
+  // // const locationElem = document.createElement('h2');
+  // // sectionElem.appendChild(locationElem);
+  // // locationElem.textContent = cookieStand.location;
+
+  // sectionElem.appendChild(hourListElem);
+
+  // // const hourListElem = document.createElement('ul');
+  // // sectionElem.appendChild(hourListElem);
+
+  // for (let i = 0; i < timeSlots.length; i += 1) {
+
+
+  //   // const hourItemElem = document.createElement('li');
+
+  //   // hourListElem.appendChild(hourItemElem);
+
+  //   // const currentTimeSlot = timeSlots[i];
+
+  //   // const currentSales = cookieStand.hourlySales[i];
+
+  //   // hourItemElem.textContent = currentTimeSlot + ': ' + currentSales + ' cookies';
+
+  // }
+
+
+  // let total = 0;
+  // // the sum of all the sales for the current cookieStand
+
+  // for (let i = 0; i < cookieStand.hourlySales.length; i += 1) {
+  //   const currentSales = cookieStand.hourlySales[i];
+  //   total += currentSales;
+  // }
+  // // need last total item
+  // const totalItemElem = document.createElement('li');
+  // hourListElem.appendChild(totalItemElem);
+  // totalItemElem.textContent = 'Total: ' + total + ' cookies';
 
 }
+renderCookieStand(locations);
